037149f749a056d02ae4777d315647cfe262df0b
jose.ekvc <jose.ekvc@gmail.com>
2020-07-11 17:43:38 -0500

    paquetes y configuración de la conexion a la DB mongo

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ import express from "express"
 import morgan from 'morgan'
 import cors from 'cors'
 import path from 'path'
+import mongoose from 'mongoose'
+
+/** 
+ * @author Jose Alonso Espinares Romero
+ * @description Conexion a la base de datos usando el ORM mongoose
+*/
+mongoose.Promise = global.Promise
+const dbURL = 'mongodb://localhost:27017/dbsistema'
+mongoose.connect(dbURL, { useCreateIndex: true, useNewUrlParser: true,useUnifiedTopology: true })
+  .then(mongoose => console.log('Conectado a la pase de datos'))
+  .catch(err => console.log(err));
+
 const app = express();
 app.use(morgan('dev'))
 app.use(cors())
