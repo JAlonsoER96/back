11b95ecf46abe53c82d6c88e04b05c0b715d8aac
jose.ekvc <jose.ekvc@gmail.com>
2020-10-01 15:05:46 -0500

    Archivo .ENV

--- server.js
+++ server.js
@@ -22,24 +22,26 @@ import router from './routes/'
 */
 mongoose.Promise = global.Promise
 const dbURL = 'mongodb://localhost:27017/dbsistema'
-mongoose.connect(dbURL, { useCreateIndex: true, useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify:false })
+mongoose.connect(dbURL, { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   .then(mongoose => console.log('Conectado a la pase de datos'))
   .catch(err => console.log(err));
 
 const app = express();
+require("dotenv").config()
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "public")))
-app.use('/api',router)
+app.use('/api', router)
 
 /**
  *@author Jose Alonso Espinares Romero
  *@description Congiguración del puerto 
  */
-var server = app.listen(8081, function () {
+const PORT = process.env.PORT || 3000
+var server = app.listen(PORT, function () {
   let host = 'localhost'
   let port = server.address().port
-    console.log("Aplicación montada en http://%s:%s", host, port);
+  console.log("Aplicación montada en http://%s:%s", host, port);
 })
